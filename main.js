0ebd44d9487430b960e4ea17016f24f4b4d3bd02
AanyaLenka <75849221+AanyaLenka@users.noreply.github.com>
2021-08-03 20:36:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ canvas.center();
 
 video = createCapture(VIDEO);
 video.hide();
+
+poseNet = ml5.poseNet(video , modelLoaded);
+poseNet.on('pose' , gotPoses);
 }
 
 function draw()
@@ -13,7 +16,15 @@ image(video,0,0,400,400);
 }
 
 song = "";
+leftWristX = "";
+leftWristY = "";
+rightWristX = "";
+rightWristY = "";
 
+function modelLoaded()
+{
+console.log("PoseNet is iniatialized");
+}
 function preload()
 {
 song = loadSound("music.mp3");
@@ -22,4 +33,31 @@ song = loadSound("music.mp3");
 function play()
 {
 song.play();
+song.setVolume(1);
+song.rate(1);
+}
+
+function gotPoses(results)
+{
+if(results.length > 0)
+{
+console.log(results);
+leftWristX = results[0].pose.leftWrist.x;
+leftWristY = results[0].pose.leftWrist.y;
+console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+
+rightWristX = results[0].pose.rightWrist.x;
+rightWristY = results[0].pose.rightWrist.y;
+console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+}
+}
+
+function stop()
+{
+song.stop();
+}
+
+function pause()
+{
+song.pause();
 }
